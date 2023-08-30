97044fce72c82221c70a370ec4052b44a98cdca4
zbahrani <139264392+zbahrani@users.noreply.github.com>
2023-08-30 11:47:15 +0330

    Add files via upload

--- class student.py
+++ class student.py
@@ -1,11 +1,15 @@
-my_leason = input("please enter your leasons(5th):  ")
+my_leason = input("please enter your leasons:  ")
 my_leason = list(my_leason.split(","))
-my_degre = input("please enter your degre(5th): ")
+my_degre = input("please enter your degre: ")
 my_degre = list(my_degre.split(","))
+if len(my_leason)!= len(my_degre) :
+    raise Exception("Sorry the entries are incorrect")
 myDict = dict(zip(my_leason, my_degre))
 my_list = list(myDict.values())
+size = len(my_list)
 from decimal import Decimal
-avrage = sum(Decimal(i) for i in my_list)/5
+avrage = sum(Decimal(i) for i in my_list)/size
+avrage = float(avrage)
 class student:
     count = 0
     def __init__(self, name, term, myDict, avrage):
@@ -18,9 +22,9 @@ class student:
     def get_term(self):
         print("term is %i" % self.term)
     def get_avrage(self):
-        print("avrage is %i" % self.avrage)
+        print("avrage is %f" % self.avrage)
     def get_info(self):
-        print("name is %s and term is %i and myDict is %s and avrage is %i" % (self.name, self.term,self.myDict, self.avrage))
+        print("name is %s and term is %i and myDict is %s and avrage is %f" % (self.name, self.term,self.myDict, self.avrage))
 
 zeinab = student("zeinab", 9, myDict, avrage)
 zeinab.get_name()
